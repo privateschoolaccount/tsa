32b894d8ec6e184df756e5fe4ca5fab0263c3f34
Skye Aquiningoc <2028.skyedavid.aquiningoc@my.caslv.org>
2026-02-01 01:43:27 +0000

    dayum bro

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,4 @@
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ~/.local/bin/yt-dlp
 chmod a+rx ~/.local/bin/yt-dlp  # Make executable
-apt-get -y update
-apt-get -y upgrade
-apt-get install -y ffmpeg
+apk update
+apk add ffmpeg-dev
