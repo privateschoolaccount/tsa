a5759fe90ef1e948b53925679511cf71de7a5023
Skye Aquiningoc <2028.skyedavid.aquiningoc@my.caslv.org>
2026-02-04 23:29:36 +0000

    i gotta poop

--- index.ts
+++ index.ts
@@ -3,8 +3,16 @@ import { swaggerUI } from '@hono/swagger-ui'
 import { describeRoute, resolver, validator,openAPIRouteHandler } from "hono-openapi";
 import { z } from "https://deno.land/x/zod@v3.25/mod.ts";
 import { Handlebars, HandlebarsConfig } from 'https://deno.land/x/handlebars/mod.ts';
-
-
+import {
+  deleteCookie,
+  getCookie,
+  getSignedCookie,
+  setCookie,
+  setSignedCookie,
+  generateCookie,
+  generateSignedCookie,
+} from 'hono/cookie'
+import {createMiddleware} from 'hono/factory'
 import {
     convertSRTToText,
     convertTextToBraille,
@@ -29,15 +37,38 @@ const handle = new Handlebars({
   helpers: undefined,
   compilerOptions: undefined,
 });
-function defaultCookieMiddleware(c:Context){
-  
-
-}
+const defaultCookieMiddleware = createMiddleware(async (c, next) =>   {
+  console.log("Checking default cookies...");
+  const existingCookies  = {
+    "cellsPerLine":getCookie(c,"cellsPerLine"),
+    "linesPerPage":getCookie(c,"linesPerPage"),
+    "table":getCookie(c,"table"),
+  }
+  const defaultCookies = {
+    "cellsPerLine":"25",
+    "linesPerPage":"30",
+    "table":"en-ueb-g2.ctb"
+  }
+  for(const key in existingCookies){
+    console.log(`Trying to set default cookie ${key}=${defaultCookies[key as keyof typeof defaultCookies]}`);
+    console.log(`Existing value: ${existingCookies[key as keyof typeof existingCookies]}`);
+    if(existingCookies[key as keyof typeof existingCookies] == undefined){
+      setCookie(c,key,defaultCookies[key as keyof typeof defaultCookies],{
+        expires:new Date(Date.now()+1000*60*60*24*365),
+      });
+      
+    }
+  }
+  await next();
+});
+app.use("*",defaultCookieMiddleware);
 app.get("/",async (c)=>{
   return c.html(await handle.renderView("pages/index"));
 })
 app.get("/settings",async (c)=>{
-  return c.html(await handle.renderView("pages/settings"));
+  return c.html(await handle.renderView("pages/settings",{
+    "return":c.req.query("return") || null,
+  }));
 })
 app.get("/share-target/",async (c)=>{
   const link = c.req.query("link");
